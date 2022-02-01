90d5e90bf7202fd2fe184ecc7b067df3c980e851
Prince Stanley Lovensky J JEAN BAPTISTE <prstanley@pih.org>
2022-02-01 07:46:24 -0500

    first java project

---
+++ src/com/prince/Main.java
@@ -0,0 +1,34 @@
+package com.prince;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    private static JFrame createGUI(){
+        JTextField input = new JTextField();
+        input.setPreferredSize(new Dimension(300, 40));
+
+        JButton convertButton = new JButton("Convert");
+        JLabel output = new JLabel();
+        output.setPreferredSize(new Dimension(300,40));
+
+        convertButton.addActionListener(actionEvent -> {
+            output.setText(TitlecaseConverter.convertToTitleCase(input.getText()));
+        });
+
+        JFrame gui = new JFrame("Title case converter");
+        gui.setLayout(new FlowLayout());
+        gui.add(input);
+        gui.add(convertButton);
+        gui.add(output);
+        gui.setLocationRelativeTo(null);
+        return gui;
+    }
+
+    public static void main(String... args){
+        JFrame application = createGUI();
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        application.setVisible(true);
+    }
+}
